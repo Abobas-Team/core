b279e8b5104bce3105a64f34d128b8ea649716b8
Bohdan Metin <great.tuna.2020@gmail.com>
2025-04-16 19:17:01 +0300

    initial commit

---
+++ src/main/java/org/core/dnd_ai/DndAiApplication.java
@@ -0,0 +1,11 @@
+package org.core.dnd_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DndAiApplication.class, args);
+    }
+}
---
+++ src/test/java/org/core/dnd_ai/DndAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.core.dnd_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
