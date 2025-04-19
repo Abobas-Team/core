3a3c3abde0e74e2a0acee6d7a8abf9296cc5f802
Bohdan Metin <great.tuna.2020@gmail.com>
2025-04-19 19:37:55 +0300

    add UserRepository

---
+++ src/main/java/org/core/dnd_ai/security/users/UserRepository.java
@@ -0,0 +1,7 @@
+package org.core.dnd_ai.security.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+}
