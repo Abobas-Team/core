7ffbf81c99bb367f6b05cf78ff2da07558c8b92f
Bohdan Metin <great.tuna.2020@gmail.com>
2025-04-20 11:57:19 +0300

    refactor handleConstrainViolation method

--- src/main/java/org/core/dnd_ai/global/GlobalExceptionHandler.java
+++ src/main/java/org/core/dnd_ai/global/GlobalExceptionHandler.java
@@ -6,12 +6,17 @@ import org.core.dnd_ai.global.exception.EmailAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private String formatFieldErrorMessage(FieldError fieldError) {
+        return String.format("%s: %s", fieldError.getField(), fieldError.getDefaultMessage());
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> handleEntityNotFoundException() {
         return ResponseEntity.notFound().build();
@@ -22,10 +27,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest()
                 .body(e.getBindingResult().getAllErrors().stream()
                         .map(error -> {
-                            var problemDetail =
-                                    ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, error.getDefaultMessage());
-                            problemDetail.setTitle("Constraint Violation");
-                            return problemDetail;
+                            var detail = error instanceof FieldError fieldError
+                                    ? ProblemDetail.forStatusAndDetail(
+                                            HttpStatus.BAD_REQUEST, formatFieldErrorMessage(fieldError))
+                                    : ProblemDetail.forStatusAndDetail(
+                                            HttpStatus.BAD_REQUEST, error.getDefaultMessage());
+                            detail.setTitle("Constraint Violation");
+                            return detail;
                         })
                         .toList());
     }
