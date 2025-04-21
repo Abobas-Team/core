1a983069a4ca4f9b03d4c2656eb6eba3dd703c7d
Bohdan Metin <great.tuna.2020@gmail.com>
2025-04-21 16:20:59 +0300

    add UserDetails interface implementation to User class

--- src/main/java/org/core/dnd_ai/security/users/User.java
+++ src/main/java/org/core/dnd_ai/security/users/User.java
@@ -3,17 +3,22 @@ package org.core.dnd_ai.security.users;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Immutable;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity(name = "users")
 @Getter
 @Setter
 @RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -49,4 +54,14 @@ public class User {
     @UpdateTimestamp
     @Setter(AccessLevel.NONE)
     private LocalDateTime lastUpdateAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
