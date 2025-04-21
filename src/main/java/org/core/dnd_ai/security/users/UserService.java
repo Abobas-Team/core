3bedd14dc06040eb09e1611ae5ee250f0b5e0d0f
Bohdan Metin <great.tuna.2020@gmail.com>
2025-04-21 16:25:54 +0300

    add UserDetailsService interface implementation to UserService class

--- src/main/java/org/core/dnd_ai/security/users/UserService.java
+++ src/main/java/org/core/dnd_ai/security/users/UserService.java
@@ -3,12 +3,15 @@ package org.core.dnd_ai.security.users;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.core.dnd_ai.global.exception.EmailAlreadyExistsException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -18,5 +21,10 @@ public class UserService {
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
     }
 }
