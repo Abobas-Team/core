bab61bc56d81552340bce3a087383791bc188c83
Bohdan Metin <great.tuna.2020@gmail.com>
2025-04-21 16:24:58 +0300

    add findByEmail signature to UserRepository

--- src/main/java/org/core/dnd_ai/security/users/UserRepository.java
+++ src/main/java/org/core/dnd_ai/security/users/UserRepository.java
@@ -1,7 +1,10 @@
 package org.core.dnd_ai.security.users;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
