d8ec31e6adedfb0d3da38d35380d79b3228bcd29
Bohdan Metin <great.tuna.2020@gmail.com>
2025-04-22 14:57:37 +0300

    add filter before to securityFilterChain

--- src/main/java/org/core/dnd_ai/security/SecurityConfig.java
+++ src/main/java/org/core/dnd_ai/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.core.dnd_ai.security;
 
 import lombok.RequiredArgsConstructor;
+import org.core.dnd_ai.security.jwt.JwtAuthFilter;
 import org.core.dnd_ai.security.users.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
@@ -23,6 +25,7 @@ import org.springframework.web.cors.CorsConfiguration;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final UserService userService;
+    private final JwtAuthFilter jwtAuthFilter;
     private final PasswordEncoder passwordEncoder;
 
     @Bean
@@ -58,6 +61,7 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated())
                 .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
