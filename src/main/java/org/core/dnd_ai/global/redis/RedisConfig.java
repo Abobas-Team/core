bc1797f6b703980b099100b326980cf618cb49c2
Bohdan Metin <great.tuna.2020@gmail.com>
2025-04-23 14:31:47 +0300

    add RedisConfig

---
+++ src/main/java/org/core/dnd_ai/global/redis/RedisConfig.java
@@ -0,0 +1,22 @@
+package org.core.dnd_ai.global.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
