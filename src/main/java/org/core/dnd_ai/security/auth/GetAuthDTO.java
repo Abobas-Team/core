8e131fe2f7a064491d7ccaf0e970b5e83b823c5d
Bohdan Metin <great.tuna.2020@gmail.com>
2025-04-29 00:38:54 +0300

    remove RefresTokenDTO

--- src/main/java/org/core/dnd_ai/security/auth/GetAuthDTO.java
+++ src/main/java/org/core/dnd_ai/security/auth/GetAuthDTO.java
@@ -1,6 +1,5 @@
 package org.core.dnd_ai.security.auth;
 
-// TODO: use HTTP-only cookie instead
 public record GetAuthDTO(
     String token
 ) {
--- src/main/java/org/core/dnd_ai/security/auth/RefresTokenDTO.java
+++
@@ -1,7 +0,0 @@
-package org.core.dnd_ai.security.auth;
-
-// TODO: use HTTP-only cookie instead
-public record RefresTokenDTO(
-    String refreshToken
-) {
-}
