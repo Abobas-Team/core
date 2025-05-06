1e0a06816d8f1241861aa3df86d2b51ac3848d09
Bohdan Metin <great.tuna.2020@gmail.com>
2025-05-06 19:21:29 +0300

    add LocalUser

---
+++ src/main/java/org/core/dnd_ai/security/oauth2/LocalUser.java
@@ -0,0 +1,28 @@
+package org.core.dnd_ai.security.oauth2;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.core.dnd_ai.security.users.Role;
+
+@Entity
+@DiscriminatorValue("LOCAL")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LocalUser extends BaseUser {
+    @NonNull
+    @NotNull
+    private String password;
+
+    public LocalUser(@NonNull String username, @NonNull String email, @NonNull Role role, @NonNull String password) {
+        super(username, email, role, AuthProvider.LOCAL);
+        this.password = password;
+    }
+
+    @Override
+    public @NonNull String getPassword() {
+        return password;
+    }
+}
