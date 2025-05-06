782262b13a5eb6a9cb08573c027a1891fbc6bedf
Bohdan Metin <great.tuna.2020@gmail.com>
2025-05-06 19:20:45 +0300

    add BaseUser

---
+++ src/main/java/org/core/dnd_ai/security/oauth2/BaseUser.java
@@ -0,0 +1,90 @@
+package org.core.dnd_ai.security.oauth2;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import lombok.*;
+import org.core.dnd_ai.security.users.Role;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DiscriminatorFormula;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity(name = "base_user")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorFormula(
+        value =
+                """
+            CASE WHEN provider = 'LOCAL' THEN 'LOCAL'
+            WHEN provider = 'GOOGLE' THEN 'OAUTH'
+            ELSE 'UNKNOWN' END
+        """,
+        discriminatorType = DiscriminatorType.STRING)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BaseUser implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @NotNull
+    @Column(unique = true)
+    private String username;
+
+    @NonNull
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NonNull
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @NonNull
+    @NotNull
+    @Setter(AccessLevel.NONE)
+    @Column(updatable = false)
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+
+    @Immutable
+    @CreationTimestamp
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createdAt;
+
+    @Immutable
+    @UpdateTimestamp
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime lastUpdateAt;
+
+    protected BaseUser(
+            @NonNull String username, @NonNull String email, @NonNull Role role, @NonNull AuthProvider provider) {
+        this.username = username;
+        this.email = email;
+        this.role = role;
+        this.provider = provider;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public @NonNull String getUsername() {
+        return username;
+    }
+}
