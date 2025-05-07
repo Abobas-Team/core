28caeab1548969152edc2315c291d735be15edd6
Bohdan Metin <great.tuna.2020@gmail.com>
2025-05-08 02:30:34 +0300

    make OAuthUser implements OAuth2User

--- src/main/java/org/core/dnd_ai/security/users/OAuthUser.java
+++ src/main/java/org/core/dnd_ai/security/users/OAuthUser.java
@@ -2,16 +2,30 @@ package org.core.dnd_ai.security.users;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Transient;
+import java.util.Map;
 import lombok.*;
 import org.core.dnd_ai.security.oauth2.AuthProvider;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Entity
-@DiscriminatorValue("GOOGLE")
+@DiscriminatorValue("OAUTH")
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class OAuthUser extends User {
+public class OAuthUser extends User implements OAuth2User {
     public OAuthUser(@NonNull String email, @NonNull Role role, @NonNull AuthProvider provider) {
         super(email, email, role, provider);
+    }
+
+    @Override
+    public String getName() {
+        return this.getUsername();
+    }
+
+    @Override
+    @Transient
+    public Map<String, Object> getAttributes() {
+        return Map.of();
     }
 }
