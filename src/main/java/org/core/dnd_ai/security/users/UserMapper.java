fe962de34aba42e2a16a5bda5d32f6e35dbf49d3
Bohdan Metin <great.tuna.2020@gmail.com>
2025-05-18 23:41:20 +0300

    add userInfo mapping to UserMapper

--- src/main/java/org/core/dnd_ai/security/users/UserMapper.java
+++ src/main/java/org/core/dnd_ai/security/users/UserMapper.java
@@ -1,11 +1,27 @@
 package org.core.dnd_ai.security.users;
 
+import org.core.dnd_ai.security.oauth2.GetUserInfoDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public abstract class UserMapper {
     public abstract LocalUser toLocalUser(PostUserDTO dto);
 
+    @Mapping(target = "userInfo", expression = "java(processUserInfo(user))")
     public abstract GetUserDTO toDTO(User user);
+
+    protected GetUserInfoDTO processUserInfo(User user) {
+        if (user instanceof OAuthUser oAuthUser) {
+            var userInfo = oAuthUser.getUserInfo();
+            return new GetUserInfoDTO(
+                    userInfo.getProvider(),
+                    userInfo.getId(),
+                    userInfo.getName(),
+                    userInfo.getEmail(),
+                    userInfo.getImage());
+        }
+        return null;
+    }
 }
