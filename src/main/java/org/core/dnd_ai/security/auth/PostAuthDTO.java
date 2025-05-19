20a32f46b190c66f19a312781bdbc920ff30e101
Bohdan Metin <great.tuna.2020@gmail.com>
2025-05-19 23:26:27 +0300

    add validation to PostAuthDTO

--- src/main/java/org/core/dnd_ai/security/auth/PostAuthDTO.java
+++ src/main/java/org/core/dnd_ai/security/auth/PostAuthDTO.java
@@ -1,7 +1,13 @@
 package org.core.dnd_ai.security.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record PostAuthDTO(
+    @NotBlank
     String username,
+    @NotBlank
+    @Size(min = 8, max = 128)
     String password
 ) {
 }
